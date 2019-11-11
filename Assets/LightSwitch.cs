791415dbab61b5b0b6e9cb96e556f388ef76b1e6
Gunjan Chandra <gunjanchandra@Gunjans-MacBook-Air.local>
2019-11-11 15:42:09 +0200

    First commit

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool change = false;
+    private Color original ;
+    void Start()
+    {
+        light = GetComponent<Light>();
+        original = light.color;
+
+    }
+
+    public Light light;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O)|| OVRInput.GetDown(OVRInput.Button.Two))
+
+        {
+            if (!change)
+            {
+                light.color = new Color(0, 0, 0);
+            }
+            else
+            {
+                light.color = original;
+
+            }
+
+            change = !change;
+
+        }
+    }
+}
---
+++ Assets/TakeMeOut.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeMeOut : MonoBehaviour
+{	
+	public CharacterController control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.control = GetComponent<CharacterController>();
+        toggle = false;
+    }
+
+    public bool toggle;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) || OVRInput.GetDown(OVRInput.Button.One))
+        {
+            toggle = !toggle;
+
+            if (toggle)
+            {
+                var pos = transform.position;
+               
+                
+                Debug.Log("Here");
+                pos.y -= 1;
+                Debug.Log(pos);
+                control.enabled = false;
+                control.transform.position = new Vector3(0,2,0);
+                control.enabled = true;
+
+            }
+            else
+            {
+                Debug.Log("Other here");
+                Debug.Log(transform.position);
+                control.enabled = false;
+                control.transform.position = new Vector3(-20, 2, -20);
+
+            }
+        }
+    }
+}
+
+
